64cc2eaa176eb8cefc0b78358f38388b60a426aa
TechDev-26 <devikarajmp@gmail.com>
2026-06-04 18:32:25 +0530

    Update add.v

--- add.v
+++ add.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 04.06.2026 17:53:23
-// Design Name: 
-// Module Name: add
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module add(s,c,a,b);
 output s,c;
